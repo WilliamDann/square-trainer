3ffa40881ecebf2c3cc98e8b80e17767e01d3176
WilliamDann <wcdann1@gmail.com>
2022-06-26 11:50:04 -0700

    inital

---
+++ src.js
@@ -0,0 +1,46 @@
+const updateUI = () => {
+    const score = document.querySelector('#correct');
+    const tries = document.querySelector('#tries');
+
+    const squareElem = document.querySelector('#square');
+    const square     = getSquare();
+
+    score.innerHTML  = document.data.score;
+    tries.innerHTML  = document.data.tries;
+    squareElem.innerHTML = `${String.fromCharCode(square.letter+96)}${square.number}`
+}
+
+const pickSquare = () => {
+    return {
+        letter: Math.ceil(Math.random() * 8),
+        number: Math.ceil(Math.random() * 8)
+    };
+}
+
+// rank is even xor file is even
+const squareIsWhite = square => 
+    (square.number % 2 == 0) ^ (square.letter % 2 == 0)
+
+const setSquare = square => document.data.square = square;
+const getSquare = ()     => document.data.square;
+
+const handleClick = isWhite => {
+    const targetSquare = getSquare();
+    const expected     = squareIsWhite(targetSquare);
+
+    document.data.tries += 1
+
+    if (isWhite == expected) {
+        document.data.score += 1
+    }
+
+    setSquare(pickSquare());
+    updateUI();
+}
+
+document.data = {
+    square : pickSquare(),
+    score  : 0,
+    tries  : 0,
+}
+updateUI();
